c96f869a7ed24184b3ca3a15a10ba3e9d9b5cc50
axeljung01 <axeljung01@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-26 09:32:55 +0000

    set login data for typo3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rpx/trunk@32572 735d13b6-9817-0410-8766-e36946ffe9aa

--- classes/Frontend/Plugin.php
+++ classes/Frontend/Plugin.php
@@ -138,7 +138,9 @@ class tx_rpx_Frontend_Plugin extends tslib_pibase {
 			$url .= '&';
 		}
 		$url .= 'logintype=login';
-		$url .= '&uident=rpx';
+		$url .= '&user=rpx';
+		$url .= '&pass=rpx';
+		
 		$encryption = t3lib_div::makeInstance('tx_rpx_Core_Encryption');
 		$params = array();
 		$params['pid'] = $this->pi_getFFvalue ( $this->cObj->data ['pi_flexform'], 'pid');
