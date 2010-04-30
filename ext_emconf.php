9886e95e12710ef2c00c5771151f4220409ee438
axeljung01 <axeljung01@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-30 13:05:48 +0000

    upload to ter
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rpx/trunk@32781 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "rpx".
 #
-# Auto generated 14-04-2010 16:47
+# Auto generated 30-04-2010 15:04
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Axel Jung',
 	'author_email' => 'axel.jung@aoemedia.de',
 	'shy' => '',
-	'dependencies' => '',
+	'dependencies' => 0,
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -29,17 +29,19 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'AOE media GmbH',
-	'version' => '0.1.dev',
+	'version' => '0.2.0',
 	'constraints' => array(
 		'depends' => array(
-			'fe_users'
+			'0' => 'fe_users',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"ba91";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"3824";s:20:"doc/wizard_form.html";s:4:"4cb3";}',
+	'_md5_values_when_last_written' => 'a:29:{s:21:"ext_conf_template.txt";s:4:"f648";s:12:"ext_icon.gif";s:4:"5414";s:17:"ext_localconf.php";s:4:"4bc0";s:14:"ext_tables.php";s:4:"a659";s:14:"ext_tables.sql";s:4:"7fc9";s:13:"locallang.xml";s:4:"00a7";s:16:"locallang_db.xml";s:4:"7fc9";s:26:"classes/Core/Connector.php";s:4:"3c2e";s:34:"classes/Core/DatabaseException.php";s:4:"a37d";s:27:"classes/Core/Encryption.php";s:4:"9aee";s:26:"classes/Core/Exception.php";s:4:"cc98";s:24:"classes/Core/Factory.php";s:4:"392a";s:24:"classes/Core/Profile.php";s:4:"eb5c";s:38:"classes/Core/UserNotFoundException.php";s:4:"91e8";s:28:"classes/Core/UserStorage.php";s:4:"9931";s:27:"classes/Frontend/Plugin.php";s:4:"0493";s:28:"classes/Frontend/Wizicon.php";s:4:"d5a5";s:29:"classes/Frontend/flexform.xml";s:4:"371f";s:30:"classes/Frontend/locallang.xml";s:4:"d204";s:24:"classes/Service/Auth.php";s:4:"6e8f";s:14:"doc/manual.sxw";s:4:"3c4c";s:14:"res/ce_wiz.gif";s:4:"02b6";s:13:"res/clear.gif";s:4:"cc11";s:34:"tests/Core/Encryption_testcase.php";s:4:"9913";s:31:"tests/Core/Factory_testcase.php";s:4:"c007";s:35:"tests/Core/UserStorage_testcase.php";s:4:"f3b0";s:32:"tests/Core/fixtures/fe_users.sql";s:4:"05c4";s:40:"tests/Core/fixtures/response_Profile.xml";s:4:"6b4d";s:31:"tests/Service/Auth_testcase.php";s:4:"7304";}',
+	'suggests' => array(
+	),
 );
 
 ?>
